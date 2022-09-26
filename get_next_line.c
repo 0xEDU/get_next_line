ef0c79f32684145bd2eb04aee4a146c2d6480b21
etachott <etachott@42sp.org.br>
2022-09-26 16:42:37 +0200

    90% done, still need to fix memleak and large values iteration

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 19:44:35 by coder             #+#    #+#             */
-/*   Updated: 2022/09/21 22:37:54 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/26 16:38:35 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,22 @@
 #include <stdio.h>
 
 static char	*set_buffer(int fd, char *buffer);
+static char	*get_line(char *buffer);
+static char	*go_to_next_line(char *buffer);
 
 char	*get_next_line(int fd)
 {
 	static char	*buf;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buf = set_buffer(fd, buf);
-	return (buf);
+	if (buf == NULL)
+		return (NULL);
+	line = get_line(buf);
+	buf = go_to_next_line(buf);
+	return (line);
 }
 
 static char	*set_buffer(int fd, char *buffer)
@@ -32,12 +39,13 @@ static char	*set_buffer(int fd, char *buffer)
 
 	i = 0;
 	tmp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (tmp == NULL)
+		return (NULL);
 	while (ft_strchr(buffer, '\n') != 1)
 	{
 		i = read(fd, tmp, BUFFER_SIZE);
 		if (i == 0 || i == -1)
 			break ;
-		tmp[i] = '\0';
 		buffer = ft_strjoin(buffer, tmp);
 		if (buffer == NULL)
 		{
@@ -46,5 +54,59 @@ static char	*set_buffer(int fd, char *buffer)
 		}
 	}
 	free(tmp);
+	if (i == -1)
+		return (NULL);
 	return (buffer);
+}
+
+static char	*get_line(char *buffer)
+{
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	if (!*(buffer + i))
+		return (NULL);
+	while (*(buffer + i) && *(buffer + i) != '\n')
+		i++;
+	line = ft_calloc(i + (*(buffer + i) == '\n') + 1, sizeof(char));
+	if (line == NULL)
+		return (NULL);
+	i = 0;
+	while (*(buffer + i) && *(buffer + i) != '\n')
+	{
+		*(line + i) = *(buffer + i);
+		i++;
+	}
+	if (*(buffer + i) == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+static char	*go_to_next_line(char *buffer)
+{
+	char	*next;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (*(buffer + i) != '\n' && *(buffer + i))
+		i++;
+	if (!*(buffer + i))
+	{
+		free(buffer);
+		return (NULL);
+	}
+	next = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	if (next == NULL)
+		return (NULL);
+	i++;
+	while (*(buffer + i + j))
+	{
+		*(next + j) = *(buffer + i + j);
+		j++;
+	}
+	free(buffer);
+	return (next);
 }
